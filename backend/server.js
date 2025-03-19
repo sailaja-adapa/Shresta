d98da1c605fa95854e5a65b70d18b4e3fcd21fb0
Sailaja Adapa <adapasailaja17@gmail.com>
2025-03-19 20:16:07 +0530

    OTP Funtionality using Twilio

--- backend/server.js
+++ backend/server.js
@@ -47,19 +47,19 @@ app.post('/send-otp', async (req, res) => {
   }
 });
 
-// Endpoint to send general SMS
+// Endpoint to send general SMS 
 app.post('/send-sms', async (req, res) => {
   const { to, body } = req.body;
   if (!to || !body) {
     return res.status(400).json({ success: false, error: 'Missing required fields: "to" and/or "body"' });
   }
+
   try {
     const message = await client.messages.create({
       body: body,
       from: twilioPhoneNumber,
       to: to
     });
-
     res.status(200).json({ success: true, messageSid: message.sid });
   } catch (error) {
     console.error('Error sending SMS:', error);
--- src/WelcomePage.jsx
+++ src/WelcomePage.jsx
@@ -1,9 +1,8 @@
-
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import GetCurrentAddress from './GetCurrentAddress';
+import GetCurrentAddress from './GetCurrentAddress';  
 import { FaHome } from "react-icons/fa";
 import { FaMapMarkerAlt } from "react-icons/fa";
 import { 
